078f294eed1985fd993aca48e4dab4731c3056fa
Devfrank94 <francescomurro94@gmail.com>
2023-06-26 22:27:48 +0200

    complete Milestone 5

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,27 @@
 <script>
-
+import ProjectCard from './partials/ProjectCard.vue';
 import { store } from '../data/store.js';
 import axios from 'axios';
 
 
 export default {
   name:'Main',
-  // data(){
-  //   return {
-    
-  //   }
-  // },
+  components:{
+    ProjectCard
+  },
+
+
+  data(){
+    return {
+      projects:[]
+    }
+  },
 
     methods:{
       getApi(){
         axios.get(store.apiUrl + 'projects')
           .then(results =>{
-
+            this.projects = results.data;
             console.log(results.data);
           })
       }
@@ -34,8 +39,16 @@ export default {
 </script>
 <template>
 
-  <div class="container my-5">
-    <h1>Main</h1>
+  <h2 class="text-center mt-2">Elenco Progetti</h2>
+  <div class="container-fluid my-5 d-flex justify-content-center align-item-center flex-wrap">
+    <ProjectCard
+      v-for="project in projects"
+      :key="project.id"
+      :title="project.title"
+      :description="project.description"
+      :image="project.image"
+      :date="project.date"
+      />
 
   </div>
 
---
+++ src/components/partials/ProjectCard.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  name:'ProjectCard',
+
+  props:{
+    title: String,
+    description: String,
+    image:String,
+    date:String,
+
+  }
+
+  
+
+}
+
+</script>
+<template>
+
+  <div class="card mx-2 my-3" style="width: 20rem;">
+      <img :src= image class="card-img-top">
+    <div class="card-body h-50">
+      <h5 class="card-title">{{title}}</h5>
+      <p class="card-text h-25">{{description}}</p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Data: {{date}}</li>
+    </ul>
+</div>
+
+  
+</template>
+
+
+<style lang="scss" scoped>
+
+  @use '../../scss/main.scss' as *;
+  //   .card{
+  //     width: calc(100% / 6);
+  //     margin-top: 2.5rem;
+  //     padding: 0 10px;
+  //     color: white;
+
+  //   //   // img{
+  //   //   //     width: 100%;
+  //   //   //     aspect-ratio: 2 / 3;
+  //   //   //     margin-bottom: .4rem;
+  //   //   // }
+
+  // }
+
+  
+  h2{
+    color: white;
+  }
+
+</style>
